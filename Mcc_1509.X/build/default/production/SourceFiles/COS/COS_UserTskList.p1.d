51a6bf8bf63a2215880ce6f00c8af64766306029
phkakaka <zkatao@163.com>
2020-06-19 13:53:04 +0800

    no message

---
+++ Mcc_1509.X/build/default/production/SourceFiles/COS/COS_UserTskList.p1.d
@@ -0,0 +1,8 @@
+build/default/production/SourceFiles/COS/COS_UserTskList.p1:  \
+SourceFiles/COS/COS_UserTskList.c  \
+SourceFiles/COS/GlobalDef.h  \
+SourceFiles/COS/COS_Defs.h  \
+SourceFiles/COS/COS_UserSetup.h  \
+SourceFiles/COS/COS_Main.h  \
+SourceFiles/COS/COS_UserTskList.h  \
+SourceFiles/COS/../SunSensor/Led.h 
---
+++ Mcc_1509.X/build/default/production/main.p1.d
@@ -0,0 +1,14 @@
+build/default/production/main.p1:  \
+main.c  \
+mcc_generated_files/mcc.h  \
+mcc_generated_files/device_config.h  \
+mcc_generated_files/pin_manager.h  \
+mcc_generated_files/interrupt_manager.h  \
+mcc_generated_files/pwm1.h  \
+mcc_generated_files/tmr2.h  \
+SourceFiles/SunSensor/Led.h  \
+SourceFiles/COS/COS_Main.h  \
+SourceFiles/COS/COS_UserTskList.h  \
+SourceFiles/COS/GlobalDef.h  \
+SourceFiles/COS/COS_Defs.h  \
+SourceFiles/COS/COS_UserSetup.h 
